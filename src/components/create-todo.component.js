d7f5c079483f2798dd95e9519a87ab7a96a77827
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-29 17:55:25 +0600

    from to database store, list show from database

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 
 class CreateTodoComponent extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
         this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
@@ -43,6 +45,17 @@ class CreateTodoComponent extends Component {
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        }
+
+        axios.post('http://localhost:5000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
 
         this.setState({
             todo_description: '',
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,10 +1,65 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+    <tr>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}> {props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+ props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 class TodosListComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { todos: []}
+
+    }
+
+    componentDidMount() {
+
+        axios.get('http://localhost:5000/todos')
+            .then(response => {
+                this.setState({ todos: response.data });
+            })
+            .catch(function (err){
+                console.log(err);
+            })
+    }
+
+    todoList(){
+
+        return this.state.todos.map(function (currentTodo, index){
+
+            return <Todo todo={currentTodo} key={index}/>
+        });
+    }
+
+
     render() {
         return (
             <div>
-                <p>Welcome to todos list component</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20}}>
+                    <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Responsible</th>
+                        <th>Priority</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.todoList()}
+                    </tbody>
+
+                </table>
             </div>
         );
     }
